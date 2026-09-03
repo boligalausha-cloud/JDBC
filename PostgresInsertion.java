fee6190e1dd9378c36fff812db21b637d3525492
boligalausha-cloud <boligalausha@gmail.com>
2026-09-03 20:52:09 +0530

    Refactor main method for database insertion

--- PostgresInsertion.java
+++ PostgresInsertion.java
@@ -1,32 +1,39 @@
 package com.tnsif.postgresql;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+
 public class PostgresInsertion {
-	   public static void main(String args[]) {
-	      Connection c = null;
-	      Statement stmt = null;
-	      try {
-	         Class.forName("org.postgresql.Driver");
-	         c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres", "root");
-	         c.setAutoCommit(false);
-	         System.out.println("Opened database successfully");
-	         stmt = c.createStatement();
-	         String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES (5, 'Suresh', 32, 'California', 20000.00 );";
-	         stmt.executeUpdate(sql);
-	         sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES (6, 'Ramesh', 25, 'Texas', 15000.00 );";
-	         stmt.executeUpdate(sql);
-	         sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES (7, 'Ram', 23, 'Norway', 20000.00 );";
-	         stmt.executeUpdate(sql);
-	         sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES (8, 'Sham', 25, 'Rich-Mond ', 65000.00 );";
-	         stmt.executeUpdate(sql);
-	         stmt.close();
-	         c.commit();
-	         c.close();
-	      } catch (Exception e) {
-	         System.err.println( e.getClass().getName()+": "+ e.getMessage() );
-	         System.exit(0);
-	      }
-	      System.out.println("Records created successfully");
-	   }
-	}
+    public static void main(String[] args) {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "root");
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES (1, 'Paul', 32, 'California', 20000.00 );";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES (2, 'Allen', 25, 'Texas', 15000.00 );";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+            stmt.executeUpdate(sql);
+
+            stmt.close();
+            c.commit();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Records created successfully");
+    }
+}
