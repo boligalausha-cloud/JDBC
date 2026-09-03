8d0cdbc2e8b6148789ea5b224c0ff764e74d9dfe
boligalausha-cloud <boligalausha@gmail.com>
2026-09-03 20:59:42 +0530

    Add PostgresUpdate class for database operations
    Implement a Java program to update salary in the COMPANY table and display records.

---
+++ PostgresUpdate.java
@@ -0,0 +1,26 @@
+package com.tnsif.postgresql;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+public class PostgresUpdate {
+   public static void main(String args[]) {
+      Connection c = null; Statement stmt = null;
+      try {
+         Class.forName("org.postgresql.Driver");
+         c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres", "root");
+         c.setAutoCommit(false); System.out.println("Opened database successfully");
+         stmt = c.createStatement();
+         String sql = "UPDATE COMPANY set SALARY = 25000.00 where ID=1;";
+         stmt.executeUpdate(sql); c.commit();
+         ResultSet rs = stmt.executeQuery("SELECT * FROM COMPANY;");
+         while (rs.next()) {
+            int id = rs.getInt("id"); String name = rs.getString("name"); int age = rs.getInt("age");
+            String address = rs.getString("address"); float salary = rs.getFloat("salary");
+            System.out.println("ID = " + id + ", NAME = " + name + ", AGE = " + age + ", ADDRESS = " + address + ", SALARY = " + salary);
+         }
+         rs.close(); stmt.close(); c.close();
+      } catch (Exception e) { System.err.println(e.getClass().getName()+": "+e.getMessage()); System.exit(0); }
+      System.out.println("Operation done successfully");
+   }
+}
